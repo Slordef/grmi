61af77ec95711dc5c99748d63b296b9cb78168bb
Maximilien Valenzano <slordef.vikamet@gmail.com>
2024-03-29 23:06:54 +0100

    chore(deps): update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,18 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
-    },
-    'extends': [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier'
-    ],
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'parser': '@typescript-eslint/parser',
-        'sourceType': 'module'
-    },
-    'plugins': [
-        '@typescript-eslint'
-    ],
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ],
-        'object-curly-spacing': [
-            2,
-            'always'
-        ]
-    }
+  env: {
+    browser: false,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {}
 };
